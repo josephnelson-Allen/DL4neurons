04568e19573e6b8a91ce48f4df8370a597e217a3
Vyassa Baratham <vbaratham@gmail.com>
2019-02-23 13:21:37 -0800

    first commit

---
+++ run.py
@@ -0,0 +1,128 @@
+from argparse import ArgumentParser
+from datetime import datetime
+
+from neuron import h, gui
+import numpy as np
+import matplotlib.pyplot as plt
+
+from stimulus import stims
+
+"""
+Requires izhi2003a.mod from:
+https://senselab.med.yale.edu/ModelDB/showmodel.cshtml?model=39948
+
+Run this script from the same directory as the compiled izhi2003a.mod
+
+NB: on OSX, run using `pythonw` (first, `conda install python.app`) for plotting to work
+"""
+
+# redefine NEURON's advance() (runs one timestep) to update the current
+h('proc advance() {nrnpython("myadvance()")}')
+def myadvance():
+    h.cell.Iin = h.stim[int(h.t/h.dt)]
+    h.fadvance()
+    
+
+def get_stim(stim_type, i):
+    return stims[stim_type][i]
+
+
+def save_nwb(outfile, stim, v):
+    pass
+
+
+def plot(args, stim, u, v):
+    if args.plot_stim:
+        plt.plot(stim)
+        plt.show()
+    
+    if args.plot_v:
+        plt.plot(v)
+        plt.show()
+
+    if args.plot_u:
+        plt.plot(u)
+        plt.show()
+
+
+def main(args):
+
+    if not any([args.plot_stim, args.plot_u, args.plot_v, args.outfile, args.force]):
+        raise ValueError("You didn't choose to plot or save anything. "
+                         + "Pass --force to continue anyways")
+    
+    start = datetime.now()
+
+    # Simulation parameters
+    h.tstop = args.tstop
+    h.steps_per_ms = 1./args.dt
+    h.stdinit() # sets h.dt based on h.steps_per_ms
+    ntimepts = int(h.tstop/h.dt)
+
+    if h.dt != args.dt:
+        raise ValueError("Invalid choice of dt")
+
+    # Define the cell
+    dummy = h.Section()
+    cell = h.Izhi2003a(0.5,sec=dummy)
+    cell.a = args.a
+    cell.b = args.b
+    cell.c = args.c
+    cell.d = args.d
+
+    # Define the stimulus
+    stim = get_stim(args.stim_type, args.stim_idx)
+
+    # Make the stimulus and cell objects accessible from hoc
+    h('objref stim')
+    h.stim = stim
+    h('objref cell')
+    h.cell = cell
+    
+    # Set up recordings of u and v
+    u = h.Vector(ntimepts)
+    u.record(cell._ref_u)
+
+    v = h.Vector(ntimepts)
+    v.record(cell._ref_V)
+
+    # Run
+    print("Running simulation for {} ms with dt = {}".format(h.tstop, h.dt))
+    print("({} total timesteps)".format(ntimepts))
+
+    h.run()
+
+    print("Time to simulate: {}".format(datetime.now() - start))
+
+    # Save to nwb
+    if args.outfile:
+        save_nwb(args.outfile, stim, v)
+
+    # Plot
+    plot(args, stim, u, v)
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--outfile', type=str, required=False, default=None
+                        help='nwb file to save to')
+    parser.add_argument('--plot-v', type=bool, action='store_true', default=False)
+    parser.add_argument('--plot-u', type=bool, action='store_true', default=False)
+    parser.add_argument('--plot-stim', type=bool, action='store_true', default=False)
+    parser.add_argument('--force', type=bool, action='store_true', default=False
+                        help="make the script run even if you don't plot or save anything")
+    
+    parser.add_argument('--tstop', type=int, default=1250)
+    parser.add_argument('--dt', type=float, default=.0025)
+
+    parser.add_argument('--a', type=float, default=0.02)
+    parser.add_argument('--b', type=float, default=0.2)
+    parser.add_argument('--c', type=float, default=-65.)
+    parser.add_argument('--d', type=float, default=2.)
+
+    parser.add_argument('--stim-type', type=str, default='ramp')
+    parser.add_argument('--stim-idx', '--stim-i', type=int, default=0)
+
+    args = parser.parse_args()
+
+    main(args)
---
+++ stimulus.py
@@ -0,0 +1,93 @@
+"""
+This file defines the stimuli we use
+"""
+import numpy as np
+
+# Defaults:
+DT = 0.0025 # simulation timestep
+PREPULSE = 2
+PULSELEN = 144
+POSTPULSE = 6
+TAU = 3
+
+class StimulusGenerator(object):
+
+    def __init__(self, *args, **kwargs):
+        self.dt = kwargs.pop('dt', DT)
+        self.prepulse = kwargs.pop('prepulse', PREPULSE)
+        self.pulselen = kwargs.pop('pulselen', PULSELEN)
+        self.postpulse = kwargs.pop('postpulse', POSTPULSE)
+
+    def _pulse(self, n_pulse, **stim_args):
+        raise NotImplementedError()
+
+    def generate(self, **stim_args):
+        n_pre = int(self.prepulse / self.dt)
+        n_pulse = int(self.pulselen / self.dt)
+        n_post = int(self.postpulse / self.dt)
+        n_tot = n_pre + n_pulse + n_post
+
+        stim = np.zeros(n_tot)
+        stim[n_pre:n_pre+n_pulse] = self._pulse(n_pulse, **stim_args)
+
+        return stim
+
+    def write_csv(self, filename, delimiter='\n', append=False, **stim_args):
+        # NOT TESTED
+        print("WARNING: StimulusGenerator.write_csv() has not been tested")
+        stim = self.generate(**stim_args)
+        mode = 'ab' if append else 'wb' # 'b' for binary mode
+        with open(filename, mode) as f:
+            np.savetxt(f, stim, delimiter=delimiter)
+    
+
+class RampGenerator(StimulusGenerator):
+
+    def _pulse(self, n_pulse, rampval=0.):
+        return np.linspace(start=0, stop=rampval, num=n_pulse)
+
+    
+class NegRampGenerator(RampGenerator):
+    def _pulse(self, n_pulse, rampval=0.):
+        return np.linspace(start=rampval, stop=0, num=n_pulse
+)
+
+    
+class StepGenerator(StimulusGenerator):
+    def _pulse(self, n_pulse, stepval):
+        return np.ones(n_pulse) * stepval
+
+class NoiseGenerator(StimulusGenerator):
+
+    def _pulse(self, n_pulse, mean=0., sd=1., tau=TAU):
+        pulse = np.zeros(n_pulse)
+        for i in range(1, n_pulse):
+            diff = ((mean - pulse[i-1])*self.dt/tau
+                    + sd*np.sqrt(2*self.dt/tau)*np.random.normal(0, 1))
+            pulse[i] = pulse[i-1] + diff
+
+        return pulse
+
+
+class SinGenerator(StimulusGenerator):
+    pass # TODO
+
+
+class ChirpGenerator(StimulusGenerator):
+    pass # TODO
+
+
+n_stims = 8
+
+rampvals = [0.3 + 0.1*i for i in range(n_stims)]
+stepvals = [0.2 + 0.007*i for i in range(n_stims)]
+noise_means = [0.26 + 0.015*i for i in range(n_stims//2)]
+sds = [0.002*(i + 1) for i in range(n_stims//2)]
+
+stims = {
+    'ramp': [RampGenerator().generate(rampval=val) for val in rampvals],
+    'neg_ramp': [NegRampGenerator().generate(rampval=-val) for val in rampvals],
+    'step': [StepGenerator().generate(stepval=val) for val in stepvals],
+    'noise': [NoiseGenerator().generate(mean=mean, sd=sd)
+              for mean, sd in zip(noise_means, sds)],
+}
