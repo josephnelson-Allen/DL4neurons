395ee5458c6a65aa2c27a934d2c9d8bd8f0a1c70
Vyassa Baratham <vbaratham@gmail.com>
2019-02-24 09:48:24 -0800

    change into correct dir before running

--- sbatch.sh
+++ sbatch.sh
@@ -7,8 +7,10 @@
 #SBATCH -C haswell
 #SBATCH --mail-user vbaratham@berkeley.edu
 #SBATCH --mail-type BEGIN,END,FAIL
-#SBATCH --output "/global/cscratch1/sd/vbaratha/izhi/runs/%j.out"
-#SBATCH --error "/global/cscratch1/sd/vbaratha/izhi/runs/%j.err"
+#SBATCH --output "/global/cscratch1/sd/vbaratha/izhi/runs/slurm/%j.out"
+#SBATCH --error "/global/cscratch1/sd/vbaratha/izhi/runs/slurm/%j.err"
+
+cd /global/cscratch1/sd/vbaratha/izhi
 
 mkdir runs/${SLURM_JOB_ID}
 OUTFILE=runs/${SLURM_JOB_ID}/data.nwb
