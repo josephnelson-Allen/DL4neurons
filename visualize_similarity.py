24e300ecbcb17957df09e071251aca4f85d3f020
Vyassa Baratham <vbaratham@berkeley.edu>
2019-04-25 20:43:29 -0700

    better cmap; normalize to exp/exp comparisons

--- visualize_similarity.py
+++ visualize_similarity.py
@@ -20,7 +20,7 @@ def histogram(exp_exp_similarity, exp_pred_similarity, sim_pred_similarity):
     pass
 
 
-def similarity_heatmap(similarity_file, param_x=0, param_y=1, exp_exp_similarity=None, nbins=20, phys=True):
+def similarity_heatmap(similarity_file, param_x=0, param_y=1, exp_exp_similarity=None, nbins=40, phys=True):
     """
     Compute heatmaps of similarity between actual/predicted traces.
     if exp_exp_similarity is specified, we normalize to its mean/stdev
@@ -33,10 +33,11 @@ def similarity_heatmap(similarity_file, param_x=0, param_y=1, exp_exp_similarity
         nsamples = infile['similarity'].shape[0]
 
         if exp_exp_similarity:
-            with h5py.File(exp_exp_similarity, 'r') as infile:
-                expexp_mean = np.average(infile['similarity'])
-                expexp_std = np.stdev(infile['similarity'])
-                # TODO: normalize incoming data
+            with h5py.File(exp_exp_similarity, 'r') as expexp_infile:
+                expexp_mean = np.average(expexp_infile['similarity'])
+                expexp_std = np.std(expexp_infile['similarity'])
+        else:
+            expexp_mean, expexp_std = 0, 1
 
         if phys:
             range_x = param_ranges[param_x]
@@ -49,15 +50,16 @@ def similarity_heatmap(similarity_file, param_x=0, param_y=1, exp_exp_similarity
             paramskey = 'unitTruth2D'
             pred_paramskey = 'unitPred2D'
 
-        all_bins_x = np.linspace(*range_x, nbins)
-        all_bins_y = np.linspace(*range_y, nbins)
+        all_bins_x = np.linspace(*range_x, nbins+1)
+        all_bins_y = np.linspace(*range_y, nbins+1)
 
-        bins_x = np.digitize(infile[paramskey][:, param_x], all_bins_x)
-        bins_y = np.digitize(infile[paramskey][:, param_y], all_bins_y)
+        bins_x = np.digitize(infile[paramskey][:, param_x], all_bins_x) - 1
+        bins_y = np.digitize(infile[paramskey][:, param_y], all_bins_y) - 1
 
-        binned_similarities = defaultdict(list)
-        for bin_x, bin_y, similarity in zip(bins_x, bins_y, infile['similarity']):
-            binned_similarities[(bin_x, bin_y)].append(similarity)
+        binned_similarities = {(binx, biny):[] for binx, biny in itertools.product(range(nbins), repeat=2)}
+        similarity = (infile['similarity'][:] - expexp_mean) / expexp_std
+        for bin_x, bin_y, simil in zip(bins_x, bins_y, similarity):
+            binned_similarities[(bin_x, bin_y)].append(simil)
 
         binned_averaged_similarity = np.zeros(shape=(nbins, nbins))
         for (bin_x, bin_y), similarities in binned_similarities.items():
@@ -76,9 +78,12 @@ def similarity_heatmap(similarity_file, param_x=0, param_y=1, exp_exp_similarity
     heatmap_ax = plt.subplot(gs[:3, :3])
     trace_axs = [plt.subplot(gs[i, 3:]) for i in range(3)]
 
-    cmap = 'hot' # TODO: Use one w/o white, use custom
-    
-    im = heatmap_ax.pcolor(all_bins_x, all_bins_y, binned_averaged_similarity, cmap=cmap)
+    cmap = plt.cm.RdGy
+    cmap.set_bad('lime')
+
+    vminmax = np.nanmax(np.abs(binned_averaged_similarity))
+    im = heatmap_ax.pcolormesh(all_bins_x, all_bins_y, binned_averaged_similarity,
+                               cmap=cmap, vmin=-vminmax, vmax=vminmax)
     plt.colorbar(im, ax=heatmap_ax)
     heatmap_ax.set_xlabel('Parameter: {}'.format(param_names[param_x]))
     heatmap_ax.set_ylabel('Parameter: {}'.format(param_names[param_y]))
@@ -93,7 +98,11 @@ def similarity_heatmap(similarity_file, param_x=0, param_y=1, exp_exp_similarity
         trace_similarity = sim._similarity(true_v, pred_v)
         ax.plot(x_axis, pred_v, linewidth=0.5, label='Predicted', color=col_pred)
         ax.plot(x_axis, true_v, linewidth=0.5, label='Actual', color=col_true)
-        ax.text(1.02, 0.8, "sim. = {0:.2f}".format(trace_similarity), transform=ax.transAxes)
+        if exp_exp_similarity:
+            txt = "rel. sim = {0:.2f}".format((trace_similarity - expexp_mean)/expexp_std)
+        else:
+            txt = "sim. = {0:.2f}".format(trace_similarity)
+        ax.text(1.02, 0.8, txt, transform=ax.transAxes)
         ax.legend(bbox_to_anchor=(0.95, 0.8), loc=2)
 
         dot = Circle((true_par[param_x], true_par[param_y]), radius=0.04, color=col_true)
@@ -102,7 +111,7 @@ def similarity_heatmap(similarity_file, param_x=0, param_y=1, exp_exp_similarity
     trace_axs[0].set_xticklabels([])
     trace_axs[1].set_xticklabels([])
     trace_axs[-1].set_xlabel("Time (ms)")
-    trace_axs[-1].set_ylabel("Volts")
+    trace_axs[-1].set_ylabel("V_m")
 
     if '--save' in sys.argv:
         plt.savefig('similarity/{}_avg_similarity_{}_vs_{}.png'.format(modelname, param_names[param_x], param_names[param_y]))
@@ -111,6 +120,7 @@ def similarity_heatmap(similarity_file, param_x=0, param_y=1, exp_exp_similarity
 
 if __name__ == '__main__':
     # similarity_heatmaps('izhi', 'cellRegr.sim.pred.h5') # 'izhi_v5b_chirp_16a_blind1.answer')
-    pairs = [(0, 1), (1, 2), (2, 3), (3, 0)]
+    # pairs = [(0, 1), (1, 2), (2, 3), (3, 0)]
+    pairs = [(0, 1),]
     for x, y, in pairs:
-        similarity_heatmap('cellRegr.sim.pred_SimPredSimilarity.h5', param_x=x, param_y=y, phys=False)
+        similarity_heatmap('cellRegr.sim.pred_SimPredSimilarity.h5', exp_exp_similarity='041019A_1-ML203b_izhi_4pv6c.mpred_ExpExpSimilarity.h5', param_x=x, param_y=y, phys=False)
