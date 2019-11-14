2a0888bdadbe7a67bc2b6948f2b6ff7b12ba2205
Vyassa Baratham <vbaratham@berkeley.edu>
2019-11-13 16:49:02 -0800

    add BBP_sbatch script

---
+++ BBP_sbatch.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -l
+#SBATCH -q regular
+#SBATCH -N 1
+#SBATCH --array 1-50
+#SBATCH -t 00:30:00
+#SBATCH -J izhi
+#SBATCH -L SCRATCH,project
+#SBATCH -C haswell
+#SBATCH --mail-user vbaratham@berkeley.edu
+#SBATCH --mail-type BEGIN,END,FAIL
+#SBATCH --output "/global/cscratch1/sd/vbaratha/izhi/runs/slurm/%A_%a.out"
+#SBATCH --error "/global/cscratch1/sd/vbaratha/izhi/runs/slurm/%A_%a.err"
+
+cd /global/cscratch1/sd/vbaratha/izhi
+
+# nrnivmodl modfiles/*.mod
+
+# srun --label -n 64 python run.py --add-qa --outfile generated_data/hh_ball_stick_9param_v1_${SLURM_ARRAY_TASK_ID}_chirp_damp_16k_v1_qa.h5 --num 500000
+
+RUNDIR=runs/${SLURM_JOB_ID}
+RUNDIR=runs/${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}
+mkdir $RUNDIR
+
+M_TYPE="L5_TTPC"
+E_TYPE="cADpyr"
+DSET_NAME=${M_TYPE}_${E_TYPE}
+NSAMPLES=1024
+stimname=chirp23a.csv
+
+# OUTFILE=$RUNDIR/${DSET_NAME}_${stimname}.h5
+OUTFILE=$RUNDIR/${DSET_NAME}_${SLURM_ARRAY_TASK_ID}_${stimname}.h5
+echo "STIM FILE" $stimfile
+echo "OUTFILE" $OUTFILE
+args="--outfile $OUTFILE --stim-file ${stimfile} \
+      --model BBP --m-type ${M_TYPE} --e-type ${E_TYPE} \
+      --num $NSAMPLES --print-every 100"
+
+srun -n 1 python run.py $args --create # create output file
+stripe_medium $OUTFILE
+srun --label -n 5 python run.py $args 
+
+chmod -R a+r $RUNDIR
+
+
+
+# for stim in $(ls stims)
