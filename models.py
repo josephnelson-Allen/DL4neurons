b8b52b3259df9033f2dee08b6d67fa85a884d9b2
Vyassa Baratham <vbaratham@gmail.com>
2019-12-12 15:27:14 -0800

    do not call get_rec_pts more than once

--- models.py
+++ models.py
@@ -115,7 +115,9 @@ class BBP(BaseModel):
     STIM_MULTIPLIER = 1.0
 
     def _get_rec_pts(self):
-        return list(OrderedDict.fromkeys(get_rec_points(self.entire_cell)))
+        if not hasattr(self, 'probes'):
+            self.probes = list(OrderedDict.fromkeys(get_rec_points(self.entire_cell)))
+        return self.probes
         
     def _n_rec_pts(self):
         return len(self._get_rec_pts())
