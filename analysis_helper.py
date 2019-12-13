88321c86efeba6b5397af850b517db881070dc3e
roybens <bens.roy@gmail.com>
2019-12-13 12:30:40 -0800

    analysis_helper not working

--- analysis_helper.py
+++ analysis_helper.py
@@ -11,7 +11,16 @@ import models
 import numpy as np
 import os
 import matplotlib.pyplot as plt
-
+def make_paramset(my_model,unpredicted_params_ind,nsamples):
+    def_param_vals = my_model.DEFAULT_PARAMS
+    param_set = np.array([def_param_vals]*nsamples)
+    range_to_vary = my_model.PARAM_RANGES[unpredicted_params_ind[0]]
+    vals_check = np.linspace(range_to_vary[0],range_to_vary[1],nsamples)
+    param_set[:,unpredicted_params_ind[0]] = vals_check
+    return param_set
+    
+    
+    
 def get_stim(stimfn):
     return np.genfromtxt(stimfn, dtype=np.float32)
 stim_fn = './stims/chaotic_1.csv'
@@ -21,27 +30,23 @@ def get_volts(nsamples):
     m_type = 'L1_DAC'
     e_type ='bNAC'
     all_volts = []
-    
-    
+    unpredicted_params_ind=[0]
     my_model = get_model('BBP',log,m_type=m_type,e_type=e_type,cell_i=0) 
-    unpredicted_params_ind = [2]
-    def_param_vals = my_model.DEFAULT_PARAMS
-    param_names =  my_model.PARAM_NAMES
-    for pname,pval in zip(param_names,def_param_vals):
-        setattr(my_model, pname, pval)
-    range_to_vary = my_model.PARAM_RANGES[unpredicted_params_ind[0]]
-    vals_check = np.random.uniform(range_to_vary[0],range_to_vary[1],nsamples)
-    for pval in vals_check:
-        #my_model = get_model('BBP',log,m_type=m_type,e_type=e_type,cell_i=0) 
-        setattr(my_model, param_names[unpredicted_params_ind[0]], pval)
+    param_set = make_paramset(my_model,unpredicted_params_ind,nsamples)
+    for i in range(nsamples):
+        my_model = get_model('BBP',log,m_type,e_type,0,*param_set[i]) 
         volts = my_model.simulate(stim,0.02)
         all_volts.append(volts)
-    return all_volts
+    return volts
 def analyze_volts(all_volts):
     times = [0.02*i for i in range(len(stim))]
+    cum_sum_volts = []
     for curr_volts in all_volts:
-        volts_to_plot = curr_volts.get('bNAC219_L1_DAC_0d58fdf14a[1].axon[1]')
+        probes = list(curr_volts.keys())
+        res = [i for i in probes if 'axon' in i] 
+        volts_to_plot = curr_volts.get(res[0])
+        cum_sum_volts.append(np.cumsum(volts_to_plot))
         plt.plot(times,volts_to_plot[:-1])
     plt.show()
-all_volts = get_volts(3)
+all_volts = get_volts(5)
 analyze_volts(all_volts)
