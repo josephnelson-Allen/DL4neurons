1bc6631958f3ad647d1b81186ccf823998d03f54
roybens <bens.roy@gmail.com>
2020-01-02 14:18:54 -0800

    Update anaylze_sensitivity_cori.py

--- anaylze_sensitivity_cori.py
+++ anaylze_sensitivity_cori.py
@@ -162,13 +162,17 @@ def analyze_ecds(ECDS,def_vals,files_loc,ml_results):
         nsamples = len(curr_ecds)
         curr_mean = np.mean(curr_ecds)
         curr_std = np.std(curr_ecds)
-        
-        for l in ml_results:
-            if str(l[0])== param_names[i]:
-                ml_std =  np.sqrt((1-(l[2]/0.6)**2))
-                ml_std_raw = l[2]
-                break
-        params_sensitivity_dict[param_names[i]] = [curr_mean,curr_std,ml_std,ml_std_raw]
+        if ml_results is not None:
+            for l in ml_results:
+                if str(l[0])== param_names[i]:
+                    ml_std =  np.sqrt((1-(l[2]/0.6)**2))
+                    ml_std_raw = l[2]
+                    break
+            params_sensitivity_dict[param_names[i]] = [curr_mean,curr_std,ml_std,ml_std_raw]
+        else:
+            ml_std = []
+            ml_std_raw=[]
+            params_sensitivity_dict[param_names[i]] = [curr_mean,curr_std]
         if 'som' in param_names[i]:
             pnames_soma.append(param_names[i])
             means_soma.append(curr_mean)
@@ -276,6 +280,127 @@ def analyze_ecds(ECDS,def_vals,files_loc,ml_results):
    
     return params_sensitivity_dict
 
+def analyze_ecds_no_ML(ECDS,def_vals,files_loc):
+    ymx = 1000
+    threshold = 100
+    param_names = list(ECDS.keys())
+    pnames_soma = []
+    pnames_axon = []
+    pnames_dend = []
+    means_axon = []
+    STDs_axon = []
+    means_soma = []
+    STDs_soma = []
+    means_dend = []
+    STDs_dend = []
+    nsamples = 0
+    params_sensitivity_dict = {}
+    param_inds = range(len(param_names))
+    for i in param_inds:
+        if def_vals[i] <= 0:
+            continue
+        curr_ecds = ECDS[param_names[i]]
+        nsamples = len(curr_ecds)
+        curr_mean = np.mean(curr_ecds)
+        curr_std = np.std(curr_ecds)
+        params_sensitivity_dict[param_names[i]] = [curr_mean,curr_std]
+        if 'som' in param_names[i]:
+            pnames_soma.append(param_names[i])
+            means_soma.append(curr_mean)
+            STDs_soma.append(curr_std)
+            
+        if 'api' in param_names[i] or 'den' in param_names[i]:
+            pnames_dend.append(param_names[i])
+            means_dend.append(curr_mean)
+            STDs_dend.append(curr_std)
+        if 'axn' in param_names[i]:
+            pnames_axon.append(param_names[i])
+            means_axon.append(curr_mean)
+            STDs_axon.append(curr_std)
+    pkl_fn=files_loc + sys.argv[1] + sys.argv[2] +'mean_std_sensitivity'  +  '.pkl'
+    with open(pkl_fn, 'wb') as output:
+        pkl.dump(params_sensitivity_dict,output)
+    fig, ((ax_soma,ax_dend),( ax_axon,ax4))= plt.subplots(2,2,figsize=(15,15))
+    fig.suptitle('Sensitivity analysis mean/rms ' + sys.argv[1] + sys.argv[2])
+    
+    ax_axon.title.set_text('Axonal del_ecds')
+    means_axon = np.array(means_axon)
+    STDs_axon = np.array(STDs_axon)
+    yaxis_axon = np.divide(means_axon,STDs_axon)
+    #yaxis_axon = np.clip(yaxis_axon,0,1)
+    ax_axon.plot(range(len(pnames_axon)),yaxis_axon,'o')
+    ax_axon.set_xticks(range(len(pnames_axon)))
+    ax_axon.set_xticklabels(pnames_axon,rotation=45)
+    ax_axon.grid()
+    #ax_axon.set_ylim([0,1])
+    ax_axon.set_ylabel('avr/std')
+    
+    
+    ax_soma.title.set_text('somaal del_ecds')
+    means_soma = np.array(means_soma)
+    STDs_soma = np.array(STDs_soma)
+    yaxis_soma = np.divide(means_soma,STDs_soma)
+    #yaxis_soma = np.clip(yaxis_soma,0,1)
+    ax_soma.plot(range(len(pnames_soma)),yaxis_soma,'o')
+    ax_soma.set_xticks(range(len(pnames_soma)))
+    ax_soma.set_xticklabels(pnames_soma)
+    ax_soma.grid()
+    #ax_soma.set_ylim([0,1])
+    ax_soma.set_ylabel('avr/std')
+    
+    ax_dend.title.set_text('dendal del_ecds')
+    means_dend = np.array(means_dend)
+    STDs_dend = np.array(STDs_dend)
+    yaxis_dend = np.divide(means_dend,STDs_dend)
+    #yaxis_dend = np.clip(yaxis_dend,0,1)
+    ax_dend.plot(range(len(pnames_dend)),yaxis_dend,'o')
+    ax_dend.set_xticks(range(len(pnames_dend)))
+    ax_dend.set_xticklabels(pnames_dend)
+    ax_dend.grid()
+    #ax_dend.set_ylim([0,1])
+    ax_dend.set_ylabel('avr/std')
+    
+    
+    fig_name = sys.argv[1] + sys.argv[2]  + str(nsamples) + 'Analysis_sampling_size.pdf'
+    fig.savefig(files_loc + fig_name)
+    
+    fig1, ax = plt.subplots(1,figsize=(15,15))
+    fig1.suptitle('Sensitivity analysis RMS ' + sys.argv[1] + sys.argv[2])
+    all_STDs = np.concatenate((STDs_axon,STDs_soma,STDs_dend),axis=None) 
+    all_STDs = np.clip(all_STDs,0,ymx)
+    
+    all_pnames = pnames_axon + pnames_soma + pnames_dend
+    
+    
+    ax.title.set_text('Parameters RMS clipped at ' + str(ymx))
+    ax.plot(range(len(all_pnames)),all_STDs,'o')
+    ax.set_xticks(range(len(all_pnames)))
+    ax.set_xticklabels(all_pnames,rotation=45)
+    ax.axhline(threshold,color='red')
+    ax.axvline(len(pnames_axon) + 0.5)
+    ax.axvline(len(pnames_axon) +len(pnames_soma) + 0.5)
+    ax.grid()
+    
+    fig_name1 = sys.argv[1] + sys.argv[2]  + str(nsamples) + 'Analysis_sensitivity_threshold.pdf'
+    fig1.savefig(files_loc + fig_name1)
+    #plt.show()
+    all_means = np.concatenate((means_axon,means_soma,means_dend),axis=None)
+    excl_header = ['param_name', 'Mean ECD','STD ECD']
+#    params_sensitivity_dict_csv = {}
+#    params_sensitivity_dict_csv['param_name'] = all_pnames
+#    params_sensitivity_dict_csv['Mean_ECD'] = all_means
+#    params_sensitivity_dict_csv['STD_ECD'] = all_STDs
+#    params_sensitivity_dict_csv['Adj_ML_STD'] = raw_ml_stds
+    
+    excl_fn=files_loc + 'sensitivity'  + sys.argv[1] + sys.argv[2] + '.csv'
+    with open(excl_fn, 'w',newline='') as out_file:
+        writer = csv.writer(out_file)
+        writer.writerow(excl_header)
+        for (pname,mean,std,) in  zip(all_pnames,all_means,all_STDs):
+            writer.writerow([pname,mean,std])
+   
+    return params_sensitivity_dict
+
 def main():
     short_name = None
     m_type = sys.argv[1]
@@ -291,8 +416,11 @@ def main():
     
     ECDS = test_sensitivity(files_loc,my_model)
     #if short_name is not  None:
-    ml_results = get_ml_results(short_name,list(ECDS.keys())) 
-    analyze_ecds(ECDS,def_vals,files_loc,ml_results)
+    if short_name is not None:
+        ml_results = get_ml_results(short_name,list(ECDS.keys())) 
+        analyze_ecds(ECDS,def_vals,files_loc,ml_results)
+    else:
+        analyze_ecds_no_ML(ECDS,def_vals,files_loc)
     
     
 main()
